d7ab170f794d981f867d64705d66afb249a1cbdc
Shunya Sasakawa <57789955+sorakawa101@users.noreply.github.com>
2023-11-12 12:58:56 +0900

    Revert "Feature/ver1/sidebar: サイドバーの雛形について，入力欄に対する入出力を確認"

--- index.py
+++ index.py
@@ -1,48 +1,24 @@
 import streamlit as st
 import functions.func_common as fc
-import functions.func_sidebar as fs
 
 
 # * サイドバーコンテンツ
 def sidebar():
     with st.sidebar:
-        st.header("Navigation")
+        st.subheader("Navigation")
         # マルチセレクト（動画・画像・逐次・全体）
-        navigation = fs.input_navigation()
-        fs.set_value_to_the_session_state("navigation", navigation)
-
-        st.header("Layout")
+        st.subheader("Layout")
         # ラジオボタン（レイアウト）
-        layout = fs.input_layout()
-        fs.set_value_to_the_session_state("layout", layout)
-
         # スライダー（フォントサイズ）
-        font_size = fs.input_font_size()
-        fs.set_value_to_the_session_state("font_size", font_size)
-
         # カラーピッカー（色）
-        color = fs.input_color()
-        fs.set_value_to_the_session_state("color", color)
-
-        st.header("Task-Feature Set")
+        st.subheader("Task-Feature Set")
         # トグル（情報量最大・最小）
-        task_feature_set = fs.input_task_feature_set()
-        fs.set_value_to_the_session_state("task_feature_set", task_feature_set)
-
-
-def container():
-    st.write(fs.get_value_from_the_session_state("navigation"))
-    st.write(fs.get_value_from_the_session_state("layout"))
-    st.write(fs.get_value_from_the_session_state("font_size"))
-    st.write(fs.get_value_from_the_session_state("color"))
-    st.write(fs.get_value_from_the_session_state("task_feature_set"))
 
 
 # main関数
 def main():
-    fc.set_page_config()
+    fc.setPageConfig()
     sidebar()
-    container()
 
 
 # 実行
